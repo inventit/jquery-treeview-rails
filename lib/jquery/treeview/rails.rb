e7c394c8a8ac8a30b94a4c338942b377fa239e92
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 15:17:28 +0900

    initial commit

---
+++ lib/jquery/treeview/rails.rb
@@ -0,0 +1,10 @@
+require "jquery/treeview/rails/version"
+
+module Jquery
+  module Treeview
+    module Rails
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jquery/treeview/rails/version.rb
@@ -0,0 +1,7 @@
+module Jquery
+  module Treeview
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
